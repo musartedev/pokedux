75852a516ed20fdd924560b1bf012c5e6921cf52
Mariangelica Useche <mariangelica.useche@gmail.com>
2021-11-28 17:02:29 +0100

    Add useSelector

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,6 +1,6 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import Searcher from '../../components/Searcher';
 import PokemonList from '../../components/PokemonList';
 import { getPokemons } from '../../api/getPokemons';
@@ -8,7 +8,7 @@ import { setPokemons } from '../../actions';
 import './styles.css';
 
 function Home() {
-  const pokemons = Array(20).fill();
+  const pokemons = useSelector((state) => state.list);
   const dispatch = useDispatch();
 
   useEffect(() => {
